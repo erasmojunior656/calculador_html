cb82c4a3f03cf6cdbbb7dd34141794e8bd5676c1
erasmojunior656 <j.erasmo@escola.pr.gov.br>
2025-06-26 08:00:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function subtrair(){
     document.getElementById('resultado').textContent = 'Resultado: ' + soma_numero;
 
 }
+
+function multiplicar(){
+
+    let numero_um = parseInt(document.getElementById('primeiro').value);
+    let numero_dois = parseInt(document.getElementById('segundo').value);
+
+    let soma_numero = numero_um * numero_dois;
+    
+    document.getElementById('resultado').textContent = 'Resultado: ' + soma_numero;
+}
+
+function divisao(){
+
+    let numero_um = parseFloat(document.getElementById('primeiro').value);
+    let numero_dois = parseFloat(document.getElementById('segundo').value); 
+
+    if(numero_dois == 0){
+
+    document.getElementById('resultado').textContent = 'Não pode dividir por 0!';
+    return;
+}
+
+    let soma_numero = numero_um / numero_dois;
+    
+    document.getElementById('resultado').textContent = 'Resultado: ' + soma_numero;
+
+}
